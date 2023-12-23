02e3a022762c073bed9baa29deb590d4c7493cf2
Victor Murcia <victor.murcia@wsu.edu>
2023-12-23 12:58:58 -0500

    Update dft_clusters_viz.py

--- dft_clusters_viz.py
+++ dft_clusters_viz.py
@@ -5,5 +5,10 @@ import streamlit.components.v1 as components
 html_file = open('dft_clusters.html', 'r', encoding='utf-8')
 source_code = html_file.read() 
 
+# Set up the title and description for the Streamlit app
+st.title('Interactive Visualization of DFT Clustering Algorithm')
+st.write('This interactive plot demonstrates the clustering visualization. Hover over the plot to see more details about each transition pair like the percent overlap and the cluster it belongs to.')
+st.write('This was made using Plotly, unfortunately, encoding the coloring algorithm I made was not possible to implement at the moment. But maybe in the future...)
+
 # Use Streamlit's components.html to display the plot
 components.html(source_code, width=700, height=800)
